7436fc2374061e14d93ef03843298dfc652f1e95
N0tTryHard <billy.bones1@mail.ru>
2024-02-17 11:03:44 +0300

    delete unused files

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -26,37 +26,37 @@ export default {
       const ctx = canvas.getContext('2d');
       const width = canvas.width;
       const height = canvas.height;
-        // Задаём тёмно-синий фон
-        ctx.fillStyle = 'rgba(0,0,5,0.82)'; // Или любой другой оттенок тёмно-синего
-        ctx.fillRect(0, 0, width, height);
+      // Задаём тёмно-синий фон
+      ctx.fillStyle = 'rgba(0,0,5,0.82)'; // Или любой другой оттенок тёмно-синего
+      ctx.fillRect(0, 0, width, height);
 
-        // Параметры сетки
-        const gridSpacing = 70; // Расстояние между линиями
-        const majorLineInterval = 5; // Каждая 10-я линия будет более толстой
-        const minorLineWidth = 0.5; // Толщина тонких линий
-        const majorLineWidth = 1; // Толщина толстых линий
-        const minorLineColor = '#203148'; // Цвет тонких линий
-        const majorLineColor = '#1c3348'; // Цвет толстых линий
+      // Параметры сетки
+      const gridSpacing = 70; // Расстояние между линиями
+      const majorLineInterval = 5; // Каждая 10-я линия будет более толстой
+      const minorLineWidth = 0.5; // Толщина тонких линий
+      const majorLineWidth = 1; // Толщина толстых линий
+      const minorLineColor = '#203148'; // Цвет тонких линий
+      const majorLineColor = '#1c3348'; // Цвет толстых линий
 
-        // Рисуем сетку
-        for (let x = 0; x <= width; x += gridSpacing) {
-          ctx.beginPath();
-          ctx.moveTo(x, 0);
-          ctx.lineTo(x, height);
-          ctx.lineWidth = x % (gridSpacing * majorLineInterval) === 0 ? majorLineWidth : minorLineWidth;
-          ctx.strokeStyle = x % (gridSpacing * majorLineInterval) === 0 ? majorLineColor : minorLineColor;
-          ctx.stroke();
-        }
-        for (let y = 0; y <= height; y += gridSpacing) {
-          ctx.beginPath();
-          ctx.moveTo(0, y);
-          ctx.lineTo(width, y);
-          ctx.lineWidth = y % (gridSpacing * majorLineInterval) === 0 ? majorLineWidth : minorLineWidth;
-          ctx.strokeStyle = y % (gridSpacing * majorLineInterval) === 0 ? majorLineColor : minorLineColor;
-          ctx.stroke();
-        }
+      // Рисуем сетку
+      for (let x = 0; x <= width; x += gridSpacing) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, height);
+        ctx.lineWidth = x % (gridSpacing * majorLineInterval) === 0 ? majorLineWidth : minorLineWidth;
+        ctx.strokeStyle = x % (gridSpacing * majorLineInterval) === 0 ? majorLineColor : minorLineColor;
+        ctx.stroke();
       }
-    },
+      for (let y = 0; y <= height; y += gridSpacing) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(width, y);
+        ctx.lineWidth = y % (gridSpacing * majorLineInterval) === 0 ? majorLineWidth : minorLineWidth;
+        ctx.strokeStyle = y % (gridSpacing * majorLineInterval) === 0 ? majorLineColor : minorLineColor;
+        ctx.stroke();
+      }
+    }
+  },
 };
 </script>
 
@@ -65,6 +65,7 @@ export default {
   width: 100%;
   height: 100%;
 }
+
 canvas {
   display: block; /* Убирает полосу прокрутки */
 }
