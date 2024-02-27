3a448c791668c52229d1ebec3d21fcb737f174da
андрей <andrew@MacBook-Pro.local>
2024-02-27 10:55:37 +0300

    cringe

---
+++ src/components/Card2.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="{'card2': true, 'loaded': isLoaded}">
+    <div class="rectangle2">
+      <img class="dlcImg" :src="data.imgSrc" alt="">
+    </div>
+    <div class="card2-content">
+      <h3>{{ data.title }}</h3>
+      <p>{{ data.description }}</p>
+      <a class="mi" :href="data.link" target="_blank">show me more...</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoaded: false, // Изначально контент не загружен
+    };
+  },
+  mounted() {
+    this.isLoaded = true;
+    setTimeout(this.loadData, 700);
+  },
+  methods: {
+    loadData() {
+      this.isLoaded = true;
+    }
+  },
+  props: ['data'],
+};
+</script>
+
+<style scoped>
+/* Ваши стили для Card2 */
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="sidebar" :class="{ 'loaded': isLoaded, 'expanded': isExpanded }">
+    <button @click="toggleSidebar" class="playCrossBtn"></button>
+    <div class="menu-icons">
+      <div class="menu-icon" v-for="(icon, index) in icons" :key="index">
+        <i :class="icon.class"></i>
+        <div class="menu-icon-label">{{ icon.label }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      isExpanded: false,
+      isLoaded: false,
+      icons: [
+        { class: 'fi fi-sr-play', label: 'Запустить игру' },
+        { class: 'fi fi-sr-globe', label: 'Создать комнату' },
+        { class: 'fi fi-sr-settings', label: 'Настройки' },
+      ],
+    };
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    toggleSidebar() {
+      this.isExpanded = !this.isExpanded;
+    },
+    // async loadData() {
+    //   try {
+    //     // Загрузка данных
+    //     // Предположим, это асинхронный вызов
+    //     await fetchData();
+    //     this.isLoaded = true;
+    //   } catch (error) {
+    //     console.error("Ошибка загрузки данных", error);
+    //   }
+    // },
+    loadData() {
+        this.isLoaded = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Ваши стили для Sidebar */
+</style>
