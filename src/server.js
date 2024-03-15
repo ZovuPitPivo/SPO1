2688af9e5cb16567996a4a7b278915c1c69b6fb3
N0tTryHard <billy.bones1@mail.ru>
2024-03-15 22:20:31 +0300

    rooms added.

--- src/server.js
+++ src/server.js
@@ -18,16 +18,17 @@ const db = new sqlite3.Database('database.db');
 // Создание таблицы Players, если она не существует
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS Players (PlayerID INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT NOT NULL, Password TEXT NOT NULL, Email TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS Rooms (room_id INTEGER PRIMARY KEY AUTOINCREMENT, creator_username TEXT, player_username TEXT)");
 });
 
 // Функция для генерации токена
 function generateToken(userID) {
-    return jwt.sign({ id: userID }, SECRET_KEY, { expiresIn: '24h' });
+    return jwt.sign({id: userID}, SECRET_KEY, {expiresIn: '24h'});
 }
 
 // Функция для аутентификации и генерации токена
 app.post('/login', (req, res) => {
-    const { Username, Password } = req.body;
+    const {Username, Password} = req.body;
     console.log(Username, Password);
     db.get('SELECT * FROM Players WHERE Username = ? AND Password = ?', [Username, Password], (err, row) => {
         if (err) {
@@ -36,9 +37,9 @@ app.post('/login', (req, res) => {
         }
         if (row) {
             const token = generateToken(row.PlayerID);
-            res.json({ message: 'Authenticated successfully', token });
+            res.json({message: 'Authenticated successfully', token});
         } else {
-            res.status(401).json({ error: 'Invalid username or password' });
+            res.status(401).json({error: 'Invalid username or password'});
         }
     });
 });
@@ -79,6 +80,40 @@ app.post('/players', (req, res) => {
             return;
         }
         res.json({message: 'New player added', playerId: this.lastID});
+    });
+});
+
+// Создание комнаты
+app.post('/create_room', (req, res) => {
+    const {creator_username} = req.body;
+
+    db.run("INSERT INTO Rooms (creator_username) VALUES (?)", [creator_username], function (err) {
+        if (err) {
+            return res.status(500).json({error: err.message});
+        }
+        res.json({message: 'Room created successfully'});
+    });
+});
+
+// Присоединение к комнате
+app.post('/join_room', (req, res) => {
+    const {room_id, player_username} = req.body;
+
+    db.run("UPDATE Rooms SET player_username = ? WHERE room_id = ?", [player_username, room_id], function (err) {
+        if (err) {
+            return res.status(500).json({error: err.message});
+        }
+        res.json({message: 'Joined room successfully'});
+    });
+});
+
+// Получение списка всех комнат
+app.get('/get_rooms', (req, res) => {
+    db.all("SELECT * FROM Rooms", (err, rows) => {
+        if (err) {
+            return res.status(500).json({error: err.message});
+        }
+        res.json({rooms: rows});
     });
 });
 
